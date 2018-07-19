f31265a3bd09b0f23918d7372d950c9cb10ebb45
Souji <timoqueezle@gmail.com>
2018-07-19 18:12:22 +0200

    sample overhaul

--- code-samples/popular-topics/permissions/index.js
+++ code-samples/popular-topics/permissions/index.js
@@ -8,30 +8,47 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+	if (message.author.bot || !message.content.startsWith('!')) return;
+	if (!message.channel.permissionsFor(client.user).has('SEND_MESSAGES')) {
+		return;
+	}
+	const botPermsGlobal = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+	if (!message.guild.me.permissions.has(botPermsGlobal)) {
+		return message.reply(`I need the permissions ${botPermsGlobal.join(', ')} for this demonstration to work properly`);
+	}
 	if (message.content === '!modeveryone') {
 		const everyonePerms = new Discord.Permissions(message.guild.defaultRole.permissions);
 		const newPerms = everyonePerms.add(['MANAGE_MESSAGES', 'KICK_MEMBERS']);
 		message.guild.defaultRole.setPermissions(newPerms.bitfield);
-		console.log('Added mod permissions to @everyone');
+		message.reply('Added mod permissions to `@everyone`');
 	}
-	if (message.content === '!unmodeveryone') {
+
+	else if (message.content === '!unmodeveryone') {
 		const everyonePerms = new Discord.Permissions(message.guild.defaultRole.permissions);
 		const newPerms = everyonePerms.remove(['MANAGE_MESSAGES', 'KICK_MEMBERS']);
 		message.guild.defaultRole.setPermissions(newPerms.bitfield);
-		console.log('Removed mod permissions from @everyone');
+		message.reply('Removed mod permissions from `@everyone`');
 	}
-	if (message.content === '!createmod') {
+
+	else if (message.content === '!createmod') {
+		if(message.guild.roles.some(role => role.name === 'Mod')) {
+			return message.reply('A role with the name "Mod" already exists on this server');
+		}
 		message.guild.createRole({ name: 'Mod', permissions: ['MANAGE_MESSAGES', 'KICK_MEMBERS'] });
-		console.log('Created a modrole');
+		message.reply('Created modrole');
 	}
+
 	else if (message.content === '!makeprivate') {
+		if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
+			return message.reply('Please make sure i have the permissions MANAGE_ROLES in this channel and retry');
+		}
 		message.channel.overwritePermissions(message.channel.guild.id, { VIEW_CHANNEL: false });
 		message.channel.overwritePermissions(message.channel.guild.me, { VIEW_CHANNEL: true });
 		message.channel.overwritePermissions(message.author.id, { VIEW_CHANNEL: true });
-		console.log(`Made channel ${message.channel.name} private`);
+		message.reply(`Made channel ${message.channel.name} private`);
 	}
 	else if (message.content === '!createprivate') {
-		message.guild.createChannel('new-channel', 'text', [{
+		message.guild.createChannel('private', 'text', [{
 			id: message.guild.id,
 			deny: ['VIEW_CHANNEL'],
 		},
@@ -43,14 +60,19 @@ client.on('message', message => {
 			id: message.client.user.id,
 			allow: ['VIEW_CHANNEL'],
 		}]);
-		console.log('Created a private channel');
+		message.reply('Created a private channel');
 	}
 	else if (message.content === '!mypermissions') {
 		const finalPermissions = message.channel.permissionsFor(message.member);
-		message.channel.send(util.inspect(finalPermissions.serialize()), { code: 'js' });
+		message.reply(util.inspect(finalPermissions.serialize()), { code: 'js' });
 	}
 	else if (message.content === '!sync') {
-		if (!message.channel.parent) return;
+		if (!message.channel.parent) {
+			return message.reply('This channel is not placed under a category');
+		}
+		if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
+			return message.reply('Please make sure i have the permissions MANAGE_ROLES in this channel and retry');
+		}
 		for (const overwrite of message.channel.permissionOverwrites.values()) {
 			if (!message.channel.parent.permissionOverwrites.has(overwrite.id)) { overwrite.delete(); }
 		}
@@ -66,9 +88,9 @@ client.on('message', message => {
 
 			message.channel.overwritePermissions(overwrite.id, options);
 		}
-		console.log(`Synchronized overwrites of ${message.channel.name} with ${message.channel.parent.name}`);
+		message.reply(`Synchronized overwrites of ${message.channel.name} with ${message.channel.parent.name}`);
 	}
-	else if (message.content === '!rolpermissions') {
+	else if (message.content === '!rolepermissions') {
 		const getRoleFinalPermissions = (channel, role) => {
 			let permissions = role.permissions | role.guild.defaultRole.permissions;
 			if (permissions & 1 << 3) {return new Discord.Permissions(permissions);}
@@ -84,7 +106,7 @@ client.on('message', message => {
 			}
 			return new Discord.Permissions(permissions);
 		};
-		message.channel.send(util.inspect(getRoleFinalPermissions(message.channel, message.member.highestRole).serialize()), { code: 'js' });
+		message.reply(util.inspect(getRoleFinalPermissions(message.channel, message.member.highestRole).serialize()), { code: 'js' });
 	}
 });
 
