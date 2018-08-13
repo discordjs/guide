9ce9e9e328fa06e47765188132eed357689cde0e
Sanctuary <sanc.pw@gmail.com>
2018-08-13 19:21:35 -0400

    Add Discord message(s) components, along with CSS and fonts needed

--- guide/assets/js/main.js
+++ guide/assets/js/main.js
@@ -1,1 +1,47 @@
-// ...
+Vue.component('discord-messages', {
+	template: '\
+		<div class="discord-messages">\
+			<slot></slot>\
+		</div>\
+	',
+});
+
+Vue.component('discord-message', {
+	template: '\
+		<div class="discord-message">\
+			<div class="author-avatar">\
+				<img :src="avatarSrc" :alt="author" />\
+			</div>\
+			<div class="message-content">\
+				<div class="author-header">\
+					<span class="author-username">{{ author }}</span>\
+				</div>\
+				<div class="message-body">\
+					<slot></slot>\
+				</div>\
+			</div>\
+		</div>\
+	</div>\
+	',
+
+	props: {
+		author: String,
+		avatar: String,
+	},
+
+	data: function() {
+		return {
+			avatarSrc: '',
+		};
+	},
+
+	created: function() {
+		var defaultAvatars = {
+			user: '/assets/img/discord-avatar-user.png',
+			blue: '/assets/img/discord-avatar-blue.png',
+			green: '/assets/img/discord-avatar-green.png',
+		};
+
+		this.avatarSrc = defaultAvatars[this.avatar] || this.avatar || defaultAvatars.blue;
+	},
+});
