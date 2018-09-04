46eb19c9ed01e61f5fc3b627e3850be7eb690838
Souji <timoqueezle@gmail.com>
2018-09-04 11:30:25 +0200

    chore: change example coding stylefor more consistency

--- code-samples/popular-topics/permissions/index.js
+++ code-samples/popular-topics/permissions/index.js
@@ -8,10 +8,13 @@ client.on('ready', () => {
 
 client.on('message', message => {
 	if (message.author.bot || !message.content.startsWith('!')) return;
+
 	if (!message.channel.permissionsFor(client.user).has('SEND_MESSAGES')) {
 		return;
 	}
+
 	const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
 	if (!message.guild.me.permissions.has(botPerms)) {
 		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
 	}
@@ -19,6 +22,7 @@ client.on('message', message => {
 	if (message.content === '!modeveryone') {
 		const everyonePerms = new Permissions(message.guild.defaultRole.permissions);
 		const newPerms = everyonePerms.add(['MANAGE_MESSAGES', 'KICK_MEMBERS']);
+
 		message.guild.defaultRole.setPermissions(newPerms.bitfield)
 			.then(() => message.reply('Added mod permissions to `@everyone`'))
 			.catch(console.error);
@@ -27,6 +31,7 @@ client.on('message', message => {
 	else if (message.content === '!unmodeveryone') {
 		const everyonePerms = new Permissions(message.guild.defaultRole.permissions);
 		const newPerms = everyonePerms.remove(['MANAGE_MESSAGES', 'KICK_MEMBERS']);
+
 		message.guild.defaultRole.setPermissions(newPerms.bitfield)
 			.then(() => message.reply('Removed mod permissions from `@everyone`'))
 			.catch(console.error);
@@ -36,6 +41,7 @@ client.on('message', message => {
 		if(message.guild.roles.some(role => role.name === 'Mod')) {
 			return message.reply('A role with the name "Mod" already exists on this server');
 		}
+
 		message.guild.createRole({ name: 'Mod', permissions: ['MANAGE_MESSAGES', 'KICK_MEMBERS'] })
 			.then(() => message.reply('Created modrole'))
 			.catch(console.error);
@@ -45,6 +51,7 @@ client.on('message', message => {
 		if (message.member.roles.some(thisRole => thisRole.name === 'Mod')) {
 			return message.reply('You have a role called Mod');
 		}
+
 		message.reply('You do not have a role called Mod');
 	}
 
@@ -52,13 +59,15 @@ client.on('message', message => {
 		if (message.member.hasPermission('KICK_MEMBERS')) {
 			return message.reply('You can kick members');
 		}
-		return message.reply('You can not kick members');
+
+		message.reply('You can not kick members');
 	}
 
 	else if (message.content === '!makeprivate') {
 		if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
 			return message.reply('Please make sure i have the permissions MANAGE_ROLES in this channel and retry');
 		}
+
 		message.channel.replacePermissionOverwrites({
 			overwrites: [
 				{
@@ -100,6 +109,7 @@ client.on('message', message => {
 		if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
 			return message.reply('Please make sure i have the permissions MANAGE_ROLES in this channel and retry');
 		}
+
 		message.channel.permissionOverwrites.get(message.guild.id).delete()
 			.then(() => message.reply(`Made channel ${message.channel.name} public`))
 			.catch(console.error);
@@ -107,6 +117,7 @@ client.on('message', message => {
 
 	else if (message.content === '!mypermissions') {
 		const finalPermissions = message.channel.permissionsFor(message.member);
+
 		message.reply(util.inspect(finalPermissions.serialize()), { code: 'js' });
 	}
 
@@ -117,12 +128,14 @@ client.on('message', message => {
 		if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
 			return message.reply('Please make sure i have the permissions MANAGE_ROLES in this channel and retry');
 		}
+
 		message.channel.lockPermissions();
 		message.reply(`Synchronized overwrites of ${message.channel.name} with ${message.channel.parent.name}`);
 	}
 
 	else if (message.content === '!rolepermissions') {
 		const roleFinalPermissions = message.channel.permissionsFor(message.member.highestRole);
+
 		message.reply(util.inspect(roleFinalPermissions.serialize()), { code: 'js' });
 	}
 });
