0393373181b5fa8a7b65fa97e65a0b44cc754050
José Miguel Tajuelo Garrigós <J-888@users.noreply.github.com>
2018-09-26 02:56:38 +0200

    Using template literals

--- code-samples/creating-your-bot/configuration-files/index.js
+++ code-samples/creating-your-bot/configuration-files/index.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === config.prefix+'ping') {
+	if (message.content === `${prefix}ping`) {
 		message.channel.send('Pong.');
 	}
 });
