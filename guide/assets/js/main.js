ca0d982777ad586efbc9d510a095a212dafe1b73
Sanctuary <sanc.pw@gmail.com>
2018-12-23 02:12:27 -0500

    Switch out old Discord message components for external plugin

--- guide/assets/js/main.js
+++ guide/assets/js/main.js
@@ -1,55 +1,8 @@
 /* eslint-disable no-undef */
-Vue.component('discord-messages', {
-	template: '\
-		<div class="discord-messages">\
-			<slot></slot>\
-		</div>\
-	',
-});
-
-Vue.component('discord-message', {
-	template: '\
-		<div class="discord-message">\
-			<div class="author-avatar">\
-				<img :src="avatarSrc" :alt="author" />\
-			</div>\
-			<div class="message-content">\
-				<div class="author-header">\
-					<span class="author-username">{{ author }}</span>\
-					<span v-if="bot" class="bot-tag">Bot</span>\
-				</div>\
-				<div class="message-body">\
-					<slot></slot>\
-				</div>\
-			</div>\
-		</div>\
-	',
-
-	props: {
-		author: String,
-		avatar: String,
-		bot: Boolean,
+Vue.use(VueDiscordMessage, {
+	avatars: {
+		djs: '/assets/img/discord-avatar-djs.png',
 	},
-
-	data: function() {
-		return {
-			avatarSrc: '',
-		};
-	},
-
-	created: function() {
-		var defaultAvatars = {
-			djs: '/assets/img/discord-avatar-djs.png',
-			blue: '/assets/img/discord-avatar-blue.png',
-			green: '/assets/img/discord-avatar-green.png',
-		};
-
-		this.avatarSrc = defaultAvatars[this.avatar] || this.avatar || defaultAvatars.blue;
-	},
-});
-
-Vue.component('mention', {
-	template: '<span class="discord-mention">@<slot></slot></span>',
 });
 
 Vue.component('tip', {
