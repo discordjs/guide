4a1fc2692c00ce5d0e3477f509efffb5d7e71b30
Sanctuary <sanc.pw@gmail.com>
2019-01-03 22:51:59 -0500

    Update eslint files

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	extends: 'aqua/vue',
+	plugins: ['markdown'],
+	rules: {
+		'arrow-body-style': 'off',
+		'brace-style': ['error', 'stroustrup'],
+		'comma-dangle': ['error', 'always-multiline'],
+		'radix': ['error', 'as-needed'],
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-negated-condition': 'off',
+		'operator-linebreak': ['error', 'before'],
+		'quote-props': ['error', 'as-needed'],
+		'vue/max-attributes-per-line': ['error', {
+			singleline: 4,
+			multiline: {
+				max: 1,
+				allowFirstline: false,
+			},
+		}],
+		'vue/html-self-closing': ['error', {
+			html: {
+				void: 'always',
+				normal: 'never',
+				component: 'always',
+			},
+		}],
+		'vue/require-default-prop': 'off',
+		'vue/require-prop-types': 'off',
+	},
+};
