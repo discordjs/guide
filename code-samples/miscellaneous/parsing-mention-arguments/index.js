c69b9d0c1b201ee313fd766d34b6b0f81bf1952e
almostSouji <timoqueezle@gmail.com>
2019-07-26 08:01:46 +0200

    fix: apply #252 to code samples

--- code-samples/miscellaneous/parsing-mention-arguments/index.js
+++ code-samples/miscellaneous/parsing-mention-arguments/index.js
@@ -19,8 +19,12 @@ function getUserFromMention(mention) {
 
 // eslint-disable-next-line no-unused-vars
 function getUserFromMentionRegEx(mention) {
-	const matches = mention.match(/^<@!?(\d+)>$/);
 	// The id is the first and only match found by the RegEx.
+	const matches = mention.match(/^<@!?(\d+)>$/);
+
+	// If supplied variable was not a mention, matches will be null instead of an array.
+	if (!matches) return;
+
 	// However the first element in the matches array will be the entire mention, not just the ID,
 	// so use index 1.
 	const id = matches[1];
