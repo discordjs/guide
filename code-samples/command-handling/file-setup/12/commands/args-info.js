9edb2c941798401c808ad17829485e7aae3e77a0
almostSouji <timoqueezle@gmail.com>
2019-08-19 17:34:38 +0200

    update: command-handling code sample

---
+++ code-samples/command-handling/file-setup/12/commands/args-info.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'args-info',
+	description: 'Information about the arguments provided.',
+	execute(message, args) {
+		if (!args.length) {
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		} else if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
+
+		message.channel.send(`First argument: ${args[0]}`);
+	},
+};
