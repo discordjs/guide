ff654faca655879b42228411ab4fb789445bd5cd
almostSouji <timoqueezle@gmail.com>
2019-10-13 16:23:38 +0200

    fix(branches): make 11.5 point to 11.x

--- guide/.vuepress/branches.js
+++ guide/.vuepress/branches.js
@@ -1,5 +1,4 @@
 export default [
-	{ label: 'v11.x', version: '11.x' },
-	{ label: 'v11.5 (stable)', version: '11.5' },
+	{ label: 'v11.5 (stable)', version: '11.x' },
 	{ label: 'v12.x (master)', version: '12.x' },
 ];
