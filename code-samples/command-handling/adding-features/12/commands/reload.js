ed2e06918e9ac7c5c8cc57e5fc3486f0ca9a30a2
tipakA <emka456@onet.pl>
2019-11-04 22:16:43 +0100

    Fix code samples

--- code-samples/command-handling/adding-features/12/commands/reload.js
+++ code-samples/command-handling/adding-features/12/commands/reload.js
@@ -5,9 +5,11 @@ module.exports = {
 	execute(message, args) {
 		const commandName = args[0].toLowerCase();
 		const command = message.client.commands.get(commandName)
-      || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-		if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+		if (!command) {
+			return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+		}
 
 		delete require.cache[require.resolve(`./${commandName}.js`)];
 
