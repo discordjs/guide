563e2b27fe5b7af20a7d058241558063dd4e8bce
almostSouji <timoqueezle@gmail.com>
2020-01-19 18:47:08 +0100

    feat(v12): dynamic urls
    * use dynamic URLSs throughout the guide
    * use dynamic URLs in code samples
    * fix non-method usage of URL in code samples
    * explain dynamic in v12-changes

--- code-samples/command-handling/file-setup/12/commands/avatar.js
+++ code-samples/command-handling/file-setup/12/commands/avatar.js
@@ -3,11 +3,11 @@ module.exports = {
 	description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
 	execute(message) {
 		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL()}`);
+			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
 		}
 
 		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: ${user.displayAvatarURL()}`;
+			return `${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`;
 		});
 
 		message.channel.send(avatarList);
