e9d373e7ef4238212c5b8ab135db031cd3f626b2
Alexander Kashev <kav2k@ya.ru>
2020-05-05 18:55:26 +0200

    chore: remove minor version from branch selector

--- guide/.vuepress/branches.js
+++ guide/.vuepress/branches.js
@@ -1,11 +1,11 @@
 export default [
 	{
-		label: 'v12.x (stable)',
+		label: 'v12 (stable)',
 		version: '12.x',
 		aliases: ['12', 'stable'],
 	},
 	{
-		label: 'v11.6',
+		label: 'v11',
 		version: '11.x',
 		aliases: ['11'],
 	},
