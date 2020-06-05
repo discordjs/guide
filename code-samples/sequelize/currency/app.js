4b776d95b3991e3fbdefee803baaf04c91c91316
James <jbkroner@gmail.com>
2020-06-05 16:45:28 -0500

    update: leaderboard command now uses cache to conform with v12 content branching in guide/sequelize/currency.md

--- code-samples/sequelize/currency/app.js
+++ code-samples/sequelize/currency/app.js
@@ -88,9 +88,9 @@ client.on('message', async message => {
 	} else if (command === 'leaderboard') {
 		return message.channel.send(
 			currency.sort((a, b) => b.balance - a.balance)
-				.filter(user => client.users.has(user.user_id))
+				.filter(user => client.users.cache.has(user.user_id))
 				.first(10)
-				.map((user, position) => `(${position + 1}) ${(client.users.get(user.user_id).tag)}: ${user.balance}💰`)
+				.map((user, position) => `(${position + 1}) ${(client.users.cache.get(user.user_id).tag)}: ${user.balance}💰`)
 				.join('\n'),
 			{ code: true }
 		);
