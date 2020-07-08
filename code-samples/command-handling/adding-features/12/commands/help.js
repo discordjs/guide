8acb20ea27a652db0ce121d52653b2af13228745
Camdan Mead <cmead@compassk12.org>
2020-07-08 10:44:03 -0600

    Update help sample code to reflect changes (#423)

--- code-samples/command-handling/adding-features/12/commands/help.js
+++ code-samples/command-handling/adding-features/12/commands/help.js
@@ -12,7 +12,7 @@ module.exports = {
 
 		if (!args.length) {
 			data.push('Here\'s a list of all my commands:');
-			data.push(commands.map(command => command.name).join(', '));
+			data.push(commands.map(command => '`' + prefix + command.name + '` - ' + command.description).join('\n'));
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
 			return message.author.send(data, { split: true })
