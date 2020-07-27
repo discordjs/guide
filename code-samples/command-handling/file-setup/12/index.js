12d380249c981093d109df053e106f0281c8863f
Hayden <37120379+haydenwalker980@users.noreply.github.com>
2020-07-27 16:09:13 -0700

    Add ${client.user.tag} to startup console.log
    Might be useful if you use one folder as a template folder and don't know what your bot is logged in as

--- code-samples/command-handling/file-setup/12/index.js
+++ code-samples/command-handling/file-setup/12/index.js
@@ -13,7 +13,7 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('logged in as ${client.user.tag}, ready');
 });
 
 client.on('message', message => {
