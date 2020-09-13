30460b973f15923b33d269fea21efb7e213e83d9
Sugden <28943913+NotSugden@users.noreply.github.com>
2020-09-13 21:54:25 +0100

    fix: use error over log

--- code-samples/command-handling/adding-features/11/commands/reload.js
+++ code-samples/command-handling/adding-features/11/commands/reload.js
@@ -18,7 +18,7 @@ module.exports = {
 			message.client.commands.set(newCommand.name, newCommand);
 			message.channel.send(`Command \`${command.name}\` was reloaded!`);
 		} catch (error) {
-			console.log(error);
+			console.error(error);
 			message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
 		}
 	},
