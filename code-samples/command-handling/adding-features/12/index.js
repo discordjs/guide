c070bb45d35bf2aa4253f70b03a170d6dde22428
sayantan300 <rovelstars300@gmail.com>
2021-01-21 22:42:57 +0530

    Didn't thought you would forget adding __dirname
    Added `__dirname` for fs.readdirSync().... I just wasted 1 full hour on finding the problem.... Anyways, add this same thing to v11 folder and other command handler folders... Hope this fixes bugs for everyone. 😏

--- code-samples/command-handling/adding-features/12/index.js
+++ code-samples/command-handling/adding-features/12/index.js
@@ -5,7 +5,7 @@ const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(__dirname+'./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
