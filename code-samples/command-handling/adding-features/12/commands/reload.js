81231307ad9c2f9e68723a6a0b44b8d42aecd06e
Shubham Parihar <shubhamparihar391@gmail.com>
2021-01-24 16:06:16 +0530

    fix: remove unintentional changes to keep consistency

--- code-samples/command-handling/adding-features/12/commands/reload.js
+++ code-samples/command-handling/adding-features/12/commands/reload.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 module.exports = {
 	name: 'reload',
 	description: 'Reloads a command',
+	args: true,
 	execute(message, args) {
-		if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
 		const commandName = args[0].toLowerCase();
 		const command = message.client.commands.get(commandName)
 			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-		if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+		if (!command) {
+			return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+		}
 
 		const commandFolders = fs.readdirSync('./commands');
 
