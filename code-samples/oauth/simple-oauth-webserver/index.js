fd3456271caf4dfc829ebf6d0c7262027cae277f
Sanctuary <sanc.pw@gmail.com>
2021-04-06 20:35:35 -0400

    fix: Add error handling in express code

--- code-samples/oauth/simple-oauth-webserver/index.js
+++ code-samples/oauth/simple-oauth-webserver/index.js
@@ -7,30 +7,36 @@ app.get('/', async ({ query }, response) => {
 	const { code } = query;
 
 	if (code) {
-		const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
-			method: 'POST',
-			body: new URLSearchParams({
-				client_id: clientID,
-				client_secret: clientSecret,
-				code,
-				grant_type: 'authorization_code',
-				redirect_uri: `http://localhost:${port}`,
-				scope: 'identify',
-			}),
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-			},
-		});
+		try {
+			const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
+				method: 'POST',
+				body: new URLSearchParams({
+					client_id: clientID,
+					client_secret: clientSecret,
+					code,
+					grant_type: 'authorization_code',
+					redirect_uri: `http://localhost:${port}`,
+					scope: 'identify',
+				}),
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			});
 
-		const oauthData = await oauthResult.json();
+			const oauthData = await oauthResult.json();
 
-		const userResult = await fetch('https://discord.com/api/users/@me', {
-			headers: {
-				authorization: `${oauthData.token_type} ${oauthData.access_token}`,
-			},
-		});
+			const userResult = await fetch('https://discord.com/api/users/@me', {
+				headers: {
+					authorization: `${oauthData.token_type} ${oauthData.access_token}`,
+				},
+			});
 
-		console.log(await userResult.json());
+			console.log(await userResult.json());
+		} catch (error) {
+			// NOTE: An unauthorized token will not throw an error;
+			// it will return a 401 Unauthorized response in the try block above
+			console.error(error);
+		}
 	}
 
 	return response.sendFile('index.html', { root: '.' });
--- guide/oauth2/README.md
+++ guide/oauth2/README.md
@@ -193,23 +193,29 @@ app.get('/', async ({ query }, response) => {
 	const { code } = query;
 
 	if (code) {
-		const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
-			method: 'POST',
-			body: new URLSearchParams({
-				client_id: clientID,
-				client_secret: clientSecret,
-				code,
-				grant_type: 'authorization_code',
-				redirect_uri: `http://localhost:${port}`,
-				scope: 'identify',
-			}),
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-			},
-		});
+		try {
+			const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
+				method: 'POST',
+				body: new URLSearchParams({
+					client_id: clientID,
+					client_secret: clientSecret,
+					code,
+					grant_type: 'authorization_code',
+					redirect_uri: `http://localhost:${port}`,
+					scope: 'identify',
+				}),
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			});
 
-		const oauthData = await oauthResult.json();
-		console.log(oauthData);
+			const oauthData = await oauthResult.json();
+			console.log(oauthData);
+		} catch (error) {
+			// NOTE: An unauthorized token will not throw an error;
+			// it will return a 401 Unauthorized response in the try block above
+			console.error(error);
+		}
 	}
 
 	return response.sendFile('index.html', { root: '.' });
