3ff23a31ab69571be5767069c1503838e2da08ec
monbrey <monbrey@gmail.com>
2021-06-13 13:00:51 +1000

    feat: sidebar

--- guide/.vuepress/sidebar.ts
+++ guide/.vuepress/sidebar.ts
@@ -170,6 +170,7 @@ export default {
 				'/additional-info/collections.md',
 				'/additional-info/async-await.md',
 				'/additional-info/rest-api.md',
+        '/additional-info/changes-in-v13',
 			],
 		},
 	],
