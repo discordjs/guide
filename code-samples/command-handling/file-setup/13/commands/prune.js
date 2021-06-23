ff2b391fc60d8bfa53b4b0a7ed18e5a32d7d1d52
Adrian Castro <adrifcastr@gmail.com>
2021-06-23 14:15:22 +0200

    apply code suggestion
    Co-authored-by: Sanctuary <Danktuary@users.noreply.github.com>

--- code-samples/command-handling/file-setup/13/commands/prune.js
+++ code-samples/command-handling/file-setup/13/commands/prune.js
@@ -16,8 +16,8 @@ module.exports = {
 
 		if (amount <= 1 || amount > 100) return interaction.reply({ content: 'You need to input a number between 1 and 99.', ephemeral: true });
 		
-		await interaction.channel.bulkDelete(amount, true).catch(err => {
-			console.error(err);
+		await interaction.channel.bulkDelete(amount, true).catch(error => {
+			console.error(error);
 			interaction.reply({ content: 'There was an error trying to prune messages in this channel!', ephemeral: true });
 		});
 
