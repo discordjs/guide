857d1a7c29f014e8954a7119090def30d211e0f0
Sanctuary <sanc.pw@gmail.com>
2021-06-28 00:32:50 -0400

    refactor: Remove all v11 code samples

--- code-samples/command-handling/adding-features/11/commands/fun/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'ping',
-	description: 'Ping!',
-	cooldown: 5,
-	execute(message) {
-		message.channel.send('Pong.');
-	},
-};
--- code-samples/command-handling/file-setup/11/commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'ping',
-	description: 'Ping!',
-	execute(message) {
-		message.channel.send('Pong.');
-	},
-};
