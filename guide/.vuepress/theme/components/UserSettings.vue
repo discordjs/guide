49026f121467d26cbbb31651c008feb0058e16dc
Sanctuary <sanc.pw@gmail.com>
2021-07-04 11:12:41 -0400

    feat: Implement settings menu toggle

--- guide/.vuepress/theme/components/UserSettings.vue
+++ guide/.vuepress/theme/components/UserSettings.vue
@@ -1,7 +1,10 @@
 <template>
-	<ul class="user-settings sidebar-links">
-		<p class="sidebar-heading sidebar-item">Settings</p>
-		<ul>
+	<ul class="user-settings sidebar-links" :class="{ 'user-settings-open': showSettings }">
+		<button class="sidebar-heading sidebar-item" @click="showSettings = !showSettings">
+			Settings
+			<ChevronRight class="menu-icon" />
+		</button>
+		<ul v-if="showSettings">
 			<li>
 				<div class="sidebar-item">
 					<ThemeOptions />
@@ -14,13 +17,32 @@
 <script setup lang="ts">
 import { defineComponent, ref } from 'vue';
 import ThemeOptions from './ThemeOptions.vue';
+import ChevronRight from './icons/ChevronRight.vue';
 
-const showMenu = ref(false);
-const hideMenu = () => showMenu.value = false;
+const showSettings = ref(false);
 </script>
 
 <style lang="scss">
 .sidebar .user-settings.sidebar-links {
 	padding: 1.25rem 0.5rem 0;
+
+	button.sidebar-heading {
+		background: none;
+		display: inline-flex;
+		align-items: center;
+		border: none;
+		cursor: pointer;
+
+		.menu-icon {
+			width: 1.5em;
+			height: 1.5em;
+		}
+	}
+
+	&.user-settings-open {
+		.sidebar-heading .menu-icon {
+			transform: rotate(90deg);
+		}
+	}
 }
 </style>
