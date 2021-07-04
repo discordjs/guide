eda4be5b1a77fa1c5f5ec47f1217faedc1b16d53
Sanctuary <sanc.pw@gmail.com>
2021-07-04 12:03:17 -0400

    refactor: Use script `setup` syntax in DocsLink

--- guide/.vuepress/components/DocsLink.vue
+++ guide/.vuepress/components/DocsLink.vue
@@ -4,35 +4,27 @@
 	</a>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed, defineComponent, defineProps } from 'vue';
 
 const baseURL = 'https://discord.js.org/#/docs';
 const docsSections = ['main', 'commando', 'collection', 'rpc'];
 
-export default defineComponent({
-	name: 'DocsLink',
-	props: {
-		section: {
-			type: String,
-			'default': 'main',
-		},
-		branch: String,
-		path: {
-			type: String,
-			required: true,
-		},
+const props = defineProps({
+	section: {
+		type: String,
+		'default': 'main',
 	},
-	setup(props) {
-		const link = computed(() => {
-			const guideSection = docsSections.find(section => section === props.section) || docsSections[0];
-			const branch = guideSection === 'main' ? 'stable' : props.branch || 'master';
-			return `${baseURL}/${guideSection}/${branch}/${props.path}`;
-		});
+	branch: String,
+	path: {
+		type: String,
+		required: true,
+	},
+});
 
-		return {
-			link,
-		};
-	},
+const link = computed(() => {
+	const guideSection = docsSections.find(section => section === props.section) || docsSections[0];
+	const branch = guideSection === 'main' ? 'stable' : props.branch || 'master';
+	return `${baseURL}/${guideSection}/${branch}/${props.path}`;
 });
 </script>
