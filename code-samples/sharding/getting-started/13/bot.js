7672695dad5b6e68cf6d2c99bb22a03d307078f6
Wise2006 <45759535+Wise2006@users.noreply.github.com>
2021-07-13 18:56:26 +0300

    feat: Update code samples (1/2).

--- code-samples/sharding/getting-started/13/bot.js
+++ code-samples/sharding/getting-started/13/bot.js
@@ -1,9 +1,8 @@
-const { Client } = require('discord.js');
-const client = new Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const prefix = '!';
 
-
-client.on('message', message => {
+client.on('messageCreate', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
@@ -12,9 +11,9 @@ client.on('message', message => {
 	if (command === 'stats') {
 		const promises = [
 			client.shard.fetchClientValues('guilds.cache.size'),
-			client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)'),
+			client.shard.broadcastEval(client => client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)),
 		];
-
+		
 		return Promise.all(promises)
 			.then(results => {
 				const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
