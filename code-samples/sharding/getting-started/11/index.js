85d20ca49b2b7942accb7a598d8e2bc6d1bac188
Hooman Hassanzadeh <37535505+Scrip7@users.noreply.github.com>
2021-07-15 01:39:26 +0430

    fix: renamed `launch` event to `shardCreate`

--- code-samples/sharding/getting-started/11/index.js
+++ code-samples/sharding/getting-started/11/index.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./bot.js', { token: 'your-token-goes-here' });
 
-manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
