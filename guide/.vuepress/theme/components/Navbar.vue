cf9f7219e1568218aad9bc2167839da77ba7ea77
Sanctuary <sanc.pw@gmail.com>
2021-07-18 21:18:35 -0400

    chore(theme): Clean up Navbar template/script

--- guide/.vuepress/theme/components/Navbar.vue
+++ guide/.vuepress/theme/components/Navbar.vue
@@ -1,23 +1,11 @@
 <template>
-	<header ref="navbar" class="navbar">
+	<header class="navbar">
 		<div class="navbar-wrapper">
 			<div class="navbar-links-wrapper">
 				<div class="navbar-links-container">
-					<span ref="siteBrand">
+					<span>
 						<RouterLink :to="siteBrandLink">
-							<img
-								v-if="siteBrandLogo"
-								class="logo"
-								:src="withBase(siteBrandLogo)"
-								:alt="siteBrandTitle"
-							/>
-							<span
-								v-if="siteBrandTitle"
-								class="site-name"
-								:class="{ 'can-hide': siteBrandLogo }"
-							>
-								{{ siteBrandTitle }}
-							</span>
+							<span class="site-name">{{ siteBrandTitle }}</span>
 						</RouterLink>
 					</span>
 					<slot name="before"></slot>
@@ -25,7 +13,7 @@
 					<slot name="after"></slot>
 				</div>
 				<div class="navbar-links-container">
-					<ToggleDarkModeButton v-if="enableDarkMode" />
+					<ToggleDarkModeButton />
 					<NavbarSearch class="navbar-search" />
 					<ToggleSidebarButton @toggle="$emit('toggle-sidebar')" />
 				</div>
@@ -36,24 +24,19 @@
 
 <script setup lang="ts">
 import { computed, ref } from 'vue';
-import { useRouteLocale, useSiteLocaleData, withBase } from '@vuepress/client';
+import { useRouteLocale, useSiteLocaleData } from '@vuepress/client';
 import { useThemeLocaleData } from '@vuepress/theme-default/lib/client/composables';
 import NavbarLinks from '@vuepress/theme-default/lib/client/components/NavbarLinks.vue';
 import ToggleSidebarButton from '@vuepress/theme-default/lib/client/components/ToggleSidebarButton.vue';
 import ToggleDarkModeButton from './ToggleDarkModeButton.vue';
 
+// NOTE: May need to be renamed to `defineEmits` in a later Vue 3.x version
 defineEmit(['toggle-sidebar']);
 
 const routeLocale = useRouteLocale();
 const siteLocale = useSiteLocaleData();
 const themeLocale = useThemeLocaleData();
 
-const navbar = ref<HTMLElement | null>(null);
-
-const siteBrand = ref<HTMLElement | null>(null);
 const siteBrandLink = computed(() => themeLocale.value.home || routeLocale.value);
-const siteBrandLogo = computed(() => themeLocale.value.logo);
 const siteBrandTitle = computed(() => siteLocale.value.title);
-
-const enableDarkMode = computed(() => themeLocale.value.darkMode);
 </script>
