35285dc5e28d2c2497faa65e00362d3a42c4131e
Wise2006 <45759535+Wise2006@users.noreply.github.com>
2021-07-24 09:06:00 +0300

    fix: Config missing.

--- code-samples/creating-your-bot/configuration-files/index.js
+++ code-samples/creating-your-bot/configuration-files/index.js
@@ -1,4 +1,5 @@
 const { Client, Intents } = require('discord.js');
+const config = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', () => {
