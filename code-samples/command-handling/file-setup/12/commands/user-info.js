4140b74dd0046090602eb598d025c5c48a05e9a7
Wise2006 <45759535+Wise2006@users.noreply.github.com>
2021-07-24 08:28:59 +0300

    refactor: `Command Handling`.

--- code-samples/command-handling/file-setup/12/commands/user-info.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'user-info',
-	description: 'Display info about yourself.',
-	execute(message) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	},
-};
