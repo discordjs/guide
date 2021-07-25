25e33014889745806cf3c8ec7085c62450a1b127
Wise2006 <45759535+Wise2006@users.noreply.github.com>
2021-07-25 17:59:35 +0300

    feat: v13 `Reactions`.

--- code-samples/popular-topics/reactions/12/basic-reacting.js
+++ code-samples/popular-topics/reactions/12/basic-reacting.js
@@ -1,11 +1,11 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
+client.on('messageCreate', message => {
 	if (message.content === '!react') {
 		message.react('😄');
 	} else if (message.content === '!react-custom') {
