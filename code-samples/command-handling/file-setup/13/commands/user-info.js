cfcada976111db4d78b910f3a2d889c57b4fc08b
Wise2006 <45759535+Wise2006@users.noreply.github.com>
2021-07-26 13:43:06 +0300

    fix: Bug from #683

--- code-samples/command-handling/file-setup/13/commands/user-info.js
+++ code-samples/command-handling/file-setup/13/commands/user-info.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'user-info',
 	description: 'Display info about yourself.',
-	async exxecute(interaction) {
+	async execute(interaction) {
 		return interaction.reply(`Your username: ${interaction.user.username}\nYour ID: ${interaction.user.id}`);
 	},
 };
