39a03f1d55ad71c0d66cce905392b8aafb08f70b
Sanctuary <sanc.pw@gmail.com>
2021-07-29 19:14:26 -0400

    chore(theme): Add space to `path` self closing tag

--- guide/.vuepress/theme/components/icons/ChevronDown.vue
+++ guide/.vuepress/theme/components/icons/ChevronDown.vue
@@ -1,3 +1,3 @@
 <template>
-	<svg viewBox="0 0 24 24"><path d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42z" fill="currentColor"/></svg>
+	<svg viewBox="0 0 24 24"><path d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42z" fill="currentColor" /></svg>
 </template>
