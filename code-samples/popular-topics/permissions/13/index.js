f9ef5e79d89be3ee2bc488c091d6b362c2fc692b
Wise2006 <45759535+Wise2006@users.noreply.github.com>
2021-07-30 15:11:12 +0300

    feat: Update `Permissions`.

--- code-samples/popular-topics/permissions/13/index.js
+++ code-samples/popular-topics/permissions/13/index.js
@@ -7,58 +7,58 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('messageCreate', message => {
-	if (message.author.bot || !message.content.startsWith('!')) return;
-	if (!message.channel.permissionsFor(client.user).has('SEND_MESSAGES')) return;
+client.on('interactionCreate', interaction => {
+	if (!interaction.isCommand()) return;
+	if (!interaction.channel.permissionsFor(client.user).has('SEND_MESSAGES')) return;
 
 	const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
 
-	if (!message.guild.me.permissions.has(botPerms)) {
-		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	if (!interaction.guild.me.permissions.has(botPerms)) {
+		return interaction.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
 	}
 
-	if (message.content === '!mod-everyone') {
-		const everyonePerms = new Permissions(message.guild.roles.everyone.permissions);
+	if (interaction.commandName === 'mod-everyone') {
+		const everyonePerms = new Permissions(interaction.guild.roles.everyone.permissions);
 		const newPerms = everyonePerms.add(['MANAGE_MESSAGES', 'KICK_MEMBERS']);
 
-		message.guild.roles.everyone.setPermissions(newPerms.bitfield)
-			.then(() => message.channel.send('Added mod permissions to `@everyone`.'))
+		interaction.guild.roles.everyone.setPermissions(newPerms.bitfield)
+			.then(() => interaction.reply('Added mod permissions to `@everyone`.'))
 			.catch(console.error);
-	} else if (message.content === '!unmod-everyone') {
-		const everyonePerms = new Permissions(message.guild.roles.everyone.permissions);
+	} else if (interaction.commandName === 'unmod-everyone') {
+		const everyonePerms = new Permissions(interaction.guild.roles.everyone.permissions);
 		const newPerms = everyonePerms.remove(['MANAGE_MESSAGES', 'KICK_MEMBERS']);
 
-		message.guild.roles.everyone.setPermissions(newPerms.bitfield)
-			.then(() => message.channel.send('Removed mod permissions from `@everyone`.'))
+		interaction.guild.roles.everyone.setPermissions(newPerms.bitfield)
+			.then(() => interaction.reply('Removed mod permissions from `@everyone`.'))
 			.catch(console.error);
-	} else if (message.content === '!create-mod') {
-		if (message.guild.roles.cache.some(role => role.name === 'Mod')) {
-			return message.channel.send('A role with the name "Mod" already exists on this server.');
+	} else if (interaction.commandName === 'create-mod') {
+		if (interaction.guild.roles.cache.some(role => role.name === 'Mod')) {
+			return interaction.reply('A role with the name "Mod" already exists on this server.');
 		}
 
-		message.guild.roles.create({ name: 'Mod', permissions: ['MANAGE_MESSAGES', 'KICK_MEMBERS'] })
-			.then(() => message.channel.send('Created Mod role.'))
+		interaction.guild.roles.create({ name: 'Mod', permissions: ['MANAGE_MESSAGES', 'KICK_MEMBERS'] })
+			.then(() => interaction.reply('Created Mod role.'))
 			.catch(console.error);
-	} else if (message.content === '!check-mod') {
-		if (message.member.roles.cache.some(role => role.name === 'Mod')) {
-			return message.channel.send('You do have a role called Mod.');
+	} else if (interaction.commandName === 'check-mod') {
+		if (interaction.member.roles.cache.some(role => role.name === 'Mod')) {
+			return interaction.reply('You do have a role called Mod.');
 		}
 
-		message.channel.send('You don\'t have a role called Mod.');
-	} else if (message.content === '!can-kick') {
-		if (message.member.permissions.has('KICK_MEMBERS')) {
-			return message.channel.send('You can kick members.');
+		interaction.reply('You don\'t have a role called Mod.');
+	} else if (interaction.commandName === 'can-kick') {
+		if (interaction.member.permissions.has('KICK_MEMBERS')) {
+			return interaction.reply('You can kick members.');
 		}
 
-		message.channel.send('You cannot kick members.');
-	} else if (message.content === '!make-private') {
-		if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
-			return message.channel.send('Please make sure I have the `MANAGE_ROLES` permission in this channel and retry.');
+		interaction.reply('You cannot kick members.');
+	} else if (interaction.commandName === 'make-private') {
+		if (!interaction.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
+			return interaction.reply('Please make sure I have the `MANAGE_ROLES` permission in this channel and retry.');
 		}
 
-		message.channel.permissionOverwrites.set([
+		interaction.channel.permissionOverwrites.set([
 			{
-				id: message.guild.id,
+				id: interaction.guild.id,
 				deny: ['VIEW_CHANNEL'],
 			},
 			{
@@ -66,22 +66,22 @@ client.on('messageCreate', message => {
 				allow: ['VIEW_CHANNEL'],
 			},
 			{
-				id: message.author.id,
+				id: interaction.author.id,
 				allow: ['VIEW_CHANNEL'],
 			},
 		])
-			.then(() => message.channel.send(`Made channel ${message.channel} private.`))
+			.then(() => interaction.reply(`Made channel ${interaction.channel} private.`))
 			.catch(console.error);
-	} else if (message.content === '!create-private') {
-		message.guild.channels.create('private', {
+	} else if (interaction.commandName === 'create-private') {
+		interaction.guild.channels.create('private', {
 			type: 'GUILD_TEXT',
 			permissionOverwrites: [
 				{
-					id: message.guild.id,
+					id: interaction.guild.id,
 					deny: ['VIEW_CHANNEL'],
 				},
 				{
-					id: message.author.id,
+					id: interaction.author.id,
 					allow: ['VIEW_CHANNEL'],
 				},
 				{
@@ -90,38 +90,38 @@ client.on('messageCreate', message => {
 				},
 			],
 		})
-			.then(() => message.channel.send('Created a private channel.'))
+			.then(() => interaction.reply('Created a private channel.'))
 			.catch(console.error);
-	} else if (message.content === '!unprivate') {
-		if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
-			return message.channel.send('Please make sure I have the permissions MANAGE_ROLES in this channel and retry.');
+	} else if (interaction.commandName === 'unprivate') {
+		if (!interaction.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
+			return interaction.reply('Please make sure I have the permissions MANAGE_ROLES in this channel and retry.');
 		}
 
-		message.channel.permissionOverwrites.delete(message.guild.id)
-			.then(() => message.channel.send(`Made channel ${message.channel} public.`))
+		interaction.channel.permissionOverwrites.delete(interaction.guild.id)
+			.then(() => interaction.reply(`Made channel ${interaction.channel} public.`))
 			.catch(console.error);
-	} else if (message.content === '!my-permissions') {
-		const finalPermissions = message.channel.permissionsFor(message.member);
+	} else if (interaction.commandName === 'my-permissions') {
+		const finalPermissions = interaction.channel.permissionsFor(interaction.member);
 
-		message.channel.send({ content: `\`\`\`js\n${util.inspect(finalPermissions.serialize())}\`\`\`` });
-	} else if (message.content === '!lock-permissions') {
-		if (!message.channel.parent) {
-			return message.channel.send('This channel is not placed under a category.');
+		interaction.reply({ content: `\`\`\`js\n${util.inspect(finalPermissions.serialize())}\`\`\`` });
+	} else if (interaction.commandName === 'lock-permissions') {
+		if (!interaction.channel.parent) {
+			return interaction.reply('This channel is not placed under a category.');
 		}
 
-		if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
-			return message.channel.send('Please make sure I have the permissions MANAGE_ROLES in this channel and retry.');
+		if (!interaction.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
+			return interaction.reply('Please make sure I have the permissions MANAGE_ROLES in this channel and retry.');
 		}
 
-		message.channel.lockPermissions()
+		interaction.channel.lockPermissions()
 			.then(() => {
-				message.channel.send(`Synchronized overwrites of ${message.channel} with the \`${message.channel.parent.name}\` category.`);
+				interaction.reply(`Synchronized overwrites of ${interaction.channel} with the \`${interaction.channel.parent.name}\` category.`);
 			})
 			.catch(console.error);
-	} else if (message.content === '!role-permissions') {
-		const roleFinalPermissions = message.channel.permissionsFor(message.member.roles.highest);
+	} else if (interaction.commandName === 'role-permissions') {
+		const roleFinalPermissions = interaction.channel.permissionsFor(interaction.member.roles.highest);
 
-		message.channel.send({ content: `\`\`\`js\n${util.inspect(roleFinalPermissions.serialize())}\`\`\`` });
+		interaction.reply({ content: `\`\`\`js\n${util.inspect(roleFinalPermissions.serialize())}\`\`\`` });
 	}
 });
 
