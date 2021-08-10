83ac8ff193be92332e65939476dd5d00f728345e
PenPow <josh@penpow.dev>
2021-08-11 00:16:38 +0100

    refactor(popular-topics): Rewrite Canvas example to a slash command (#763)

--- code-samples/popular-topics/canvas/13/index.js
+++ code-samples/popular-topics/canvas/13/index.js
@@ -1,7 +1,7 @@
-const { Client, MessageAttachment, Intents } = require('discord.js');
+const { Client, Intents, MessageAttachment } = require('discord.js');
 const Canvas = require('canvas');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -18,45 +18,38 @@ const applyText = (canvas, text) => {
 	return context.font;
 };
 
-client.on('guildMemberAdd', async member => {
-	const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
-	if (!channel) return;
-
-	const canvas = Canvas.createCanvas(700, 250);
-	const context = canvas.getContext('2d');
-
-	const background = await Canvas.loadImage('./wallpaper.jpg');
-	context.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-	context.strokeStyle = '#0099ff';
-	context.strokeRect(0, 0, canvas.width, canvas.height);
-
-	context.font = '28px sans-serif';
-	context.fillStyle = '#ffffff';
-	context.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
-
-	context.font = applyText(canvas, `${member.displayName}!`);
-	context.fillStyle = '#ffffff';
-	context.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
-
-	context.beginPath();
-	context.arc(125, 125, 100, 0, Math.PI * 2, true);
-	context.closePath();
-	context.clip();
-
-	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-	context.drawImage(avatar, 25, 25, 200, 200);
-
-	const attachment = new MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-
-	channel.send({ content: `Welcome to the server, ${member}!`, files: [attachment] });
-});
-
-client.on('interactionCreate', interaction => {
+client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	if (interaction.commandName === 'join') {
-		client.emit('guildMemberAdd', interaction.member);
+	if (interaction.commandName === 'profile') {
+		const canvas = Canvas.createCanvas(700, 250);
+		const context = canvas.getContext('2d');
+
+		const background = await Canvas.loadImage('./wallpaper.jpg');
+		context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+		context.strokeStyle = '#0099ff';
+		context.strokeRect(0, 0, canvas.width, canvas.height);
+
+		context.font = '28px sans-serif';
+		context.fillStyle = '#ffffff';
+		context.fillText('Profile', canvas.width / 2.5, canvas.height / 3.5);
+
+		context.font = applyText(canvas, `${interaction.member.displayName}!`);
+		context.fillStyle = '#ffffff';
+		context.fillText(`${interaction.member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+		context.beginPath();
+		context.arc(125, 125, 100, 0, Math.PI * 2, true);
+		context.closePath();
+		context.clip();
+
+		const avatar = await Canvas.loadImage(interaction.user.displayAvatarURL({ format: 'jpg' }));
+		context.drawImage(avatar, 25, 25, 200, 200);
+
+		const attachment = new MessageAttachment(canvas.toBuffer(), 'profile-image.png');
+
+		interaction.reply({ files: [attachment] });
 	}
 });
 
