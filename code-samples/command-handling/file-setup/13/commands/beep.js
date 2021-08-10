c9373228b627723289ee2899682726dd3604b379
Adrian Castro <adrifcastr@gmail.com>
2021-08-11 01:26:55 +0200

    refactor: Rewrite command deployment and command handling (#786)

--- code-samples/command-handling/file-setup/13/commands/beep.js
+++ code-samples/command-handling/file-setup/13/commands/beep.js
@@ -1,6 +1,9 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 module.exports = {
-	name: 'beep',
-	description: 'Beep!',
+	data: new SlashCommandBuilder()
+		.setName('beep')
+		.setDescription('Beep!'),
 	async execute(interaction) {
 		return interaction.reply('Boop!');
 	},
