d7d5c4f3ae7d2b1980862127a12d52bcaff9418c
Sanctuary <Danktuary@users.noreply.github.com>
2021-09-22 11:45:09 -0400

    Apply suggestions from code review

--- code-samples/creating-your-bot/command-handling/deploy-commands.js
+++ code-samples/creating-your-bot/command-handling/deploy-commands.js
@@ -14,5 +14,5 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => { console.log('Successfully registered application commands.'); })
+	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
--- guide/creating-your-bot/creating-commands.md
+++ guide/creating-your-bot/creating-commands.md
@@ -57,7 +57,7 @@ const commands = [
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => { console.log('Successfully registered application commands.'); })
+	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
 ```
 :::
