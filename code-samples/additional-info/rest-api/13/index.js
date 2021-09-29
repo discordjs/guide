d0777f07b7f5020060de74fdf0629ea4a054152d
GodderE2D <godderseesyou@gmail.com>
2021-09-29 13:00:42 +1000

    Updated code samples to reflect with guide

--- code-samples/additional-info/rest-api/13/index.js
+++ code-samples/additional-info/rest-api/13/index.js
@@ -15,17 +15,19 @@ client.on('interactionCreate', async interaction => {
 	const { commandName } = interaction;
 
 	if (commandName === 'cat') {
+		await interaction.deferReply();
 		const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
 
-		interaction.reply({ files: [file] });
+		interaction.editReply({ files: [file] });
 	} else if (commandName === 'urban') {
+		await interaction.deferReply();
 		const term = interaction.options.getString('term');
 		const query = new URLSearchParams({ term });
 
 		const { list } = await fetch(`https://api.urbandictionary.com/v0/define?${query}`).then(response => response.json());
 
 		if (!list.length) {
-			return interaction.reply(`No results found for **${term}**.`);
+			return interaction.editReply(`No results found for **${term}**.`);
 		}
 
 		const [answer] = list;
@@ -39,7 +41,7 @@ client.on('interactionCreate', async interaction => {
 				{ name: 'Example', value: trim(answer.example, 1024) },
 				{ name: 'Rating', value: `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.` },
 			);
-		interaction.reply({ embeds: [embed] });
+		interaction.editReply({ embeds: [embed] });
 	}
 });
 
