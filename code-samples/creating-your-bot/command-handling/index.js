1a6f567763a3587b042a0375da320414ed8df0ea
Vitor <milagre.vitor@gmail.com>
2022-01-30 02:07:20 +0000

    fix(creating-your-bot): `return`/`await` code sample inconsistency (#1002)

--- code-samples/creating-your-bot/command-handling/index.js
+++ code-samples/creating-your-bot/command-handling/index.js
@@ -27,7 +27,7 @@ client.on('interactionCreate', async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
