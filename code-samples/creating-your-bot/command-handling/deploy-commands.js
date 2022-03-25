e9608c2dce2a9251fc775a06e29fd70c63309c46
Wheelwright <carpenterskeys@gmail.com>
2022-03-24 21:47:49 -0400

    Updated require statement, add path construction
    I found that these requires weren't working for me despite same relative path. This might cause fewer errors for people adapting the code.
    
    Node.js v16.14.0.
    discord.js  v13.6.0

--- code-samples/creating-your-bot/command-handling/deploy-commands.js
+++ code-samples/creating-your-bot/command-handling/deploy-commands.js
@@ -1,10 +1,12 @@
-const fs = require('node:fs');
+const path = require('path');
+const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync('commandsPath').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
