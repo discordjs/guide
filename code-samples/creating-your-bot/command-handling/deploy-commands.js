367a0a4c52985d2b89df58bac3e98fcc3cf09d0f
joshua <carpenterskeys@gmail.com>
2022-03-25 21:14:33 -0400

    added 'node:' prefix for core modules back to require statements

--- code-samples/creating-your-bot/command-handling/deploy-commands.js
+++ code-samples/creating-your-bot/command-handling/deploy-commands.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
