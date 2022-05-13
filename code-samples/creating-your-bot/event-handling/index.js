fd011de9dd202f5dbb5777ab25880e6a225cef8c
Souji <timoqueezle@gmail.com>
2022-05-13 21:26:04 +0200

    chore: semi

--- code-samples/creating-your-bot/event-handling/index.js
+++ code-samples/creating-your-bot/event-handling/index.js
@@ -10,7 +10,7 @@ const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file)
+	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
