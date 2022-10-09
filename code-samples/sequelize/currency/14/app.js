217261689d5ef8c54608a0e462df58ad4ff50662
1Computer1 <22125769+1Computer1@users.noreply.github.com>
2022-10-09 18:51:19 -0400

    refactor(currency): define standalone helper functions (#1288)

--- code-samples/sequelize/currency/14/app.js
+++ code-samples/sequelize/currency/14/app.js
@@ -10,28 +10,24 @@ const currency = new Collection();
  * You can read more about this issue On the [Sequelize issue tracker](https://github.com/sequelize/sequelize/issues/7310).
  */
 
-Reflect.defineProperty(currency, 'add', {
-	value: async (id, amount) => {
-		const user = currency.get(id);
+async function addBalance(id, amount) {
+	const user = currency.get(id);
 
-		if (user) {
-			user.balance += Number(amount);
-			return user.save();
-		}
+	if (user) {
+		user.balance += Number(amount);
+		return user.save();
+	}
 
-		const newUser = await Users.create({ user_id: id, balance: amount });
-		currency.set(id, newUser);
+	const newUser = await Users.create({ user_id: id, balance: amount });
+	currency.set(id, newUser);
 
-		return newUser;
-	},
-});
+	return newUser;
+}
 
-Reflect.defineProperty(currency, 'getBalance', {
-	value: id => {
-		const user = currency.get(id);
-		return user ? user.balance : 0;
-	},
-});
+function getBalance(id) {
+	const user = currency.get(id);
+	return user ? user.balance : 0;
+}
 
 client.once('ready', async () => {
 	const storedBalances = await Users.findAll();
@@ -42,7 +38,7 @@ client.once('ready', async () => {
 
 client.on('messageCreate', async message => {
 	if (message.author.bot) return;
-	currency.add(message.author.id, 1);
+	addBalance(message.author.id, 1);
 });
 
 client.on('interactionCreate', async interaction => {
@@ -53,7 +49,7 @@ client.on('interactionCreate', async interaction => {
 	if (commandName === 'balance') {
 		const target = interaction.options.getUser('user') || interaction.user;
 
-		return interaction.reply(`${target.tag} has ${currency.getBalance(target.id)}💰`);
+		return interaction.reply(`${target.tag} has ${getBalance(target.id)}💰`);
 	} else if (commandName === 'inventory') {
 		const target = interaction.options.getUser('user') || interaction.user;
 		const user = await Users.findOne({ where: { user_id: target.id } });
@@ -63,28 +59,28 @@ client.on('interactionCreate', async interaction => {
 
 		return interaction.reply(`${target.tag} currently has ${items.map(t => `${t.amount} ${t.item.name}`).join(', ')}`);
 	} else if (commandName === 'transfer') {
-		const currentAmount = currency.getBalance(interaction.user.id);
+		const currentAmount = getBalance(interaction.user.id);
 		const transferAmount = interaction.options.getInteger('amount');
 		const transferTarget = interaction.options.getUser('user');
 
 		if (transferAmount > currentAmount) return interaction.reply(`Sorry ${interaction.user} you don't have that much.`);
 		if (transferAmount <= 0) return interaction.reply(`Please enter an amount greater than zero, ${interaction.user}`);
 
-		currency.add(interaction.user.id, -transferAmount);
-		currency.add(transferTarget.id, transferAmount);
+		addBalance(interaction.user.id, -transferAmount);
+		addBalance(transferTarget.id, transferAmount);
 
-		return interaction.reply(`Successfully transferred ${transferAmount}💰 to ${transferTarget.tag}. Your current balance is ${currency.getBalance(interaction.user.id)}💰`);
+		return interaction.reply(`Successfully transferred ${transferAmount}💰 to ${transferTarget.tag}. Your current balance is ${getBalance(interaction.user.id)}💰`);
 	} else if (commandName === 'buy') {
 		const itemName = interaction.options.getString('item');
 		const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: itemName } } });
 
 		if (!item) return interaction.reply('That item doesn\'t exist.');
-		if (item.cost > currency.getBalance(interaction.user.id)) {
+		if (item.cost > getBalance(interaction.user.id)) {
 			return interaction.reply(`You don't have enough currency, ${interaction.user}`);
 		}
 
 		const user = await Users.findOne({ where: { user_id: interaction.user.id } });
-		currency.add(interaction.user.id, -item.cost);
+		addBalance(interaction.user.id, -item.cost);
 		await user.addItem(item);
 
 		return interaction.reply(`You've bought a ${item.name}`);
