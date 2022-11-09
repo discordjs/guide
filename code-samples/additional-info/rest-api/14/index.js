253a8796e895f1f7649717051f63460fef9d0503
Idris <idris.gamer.2006@gmail.com>
2022-11-09 16:02:16 +0100

    fix(code-sample): should be editReply

--- code-samples/additional-info/rest-api/14/index.js
+++ code-samples/additional-info/rest-api/14/index.js
@@ -18,7 +18,7 @@ client.on(Events.InteractionCreate, async interaction => {
 	if (commandName === 'cat') {
 		const catResult = await request('https://aws.random.cat/meow');
 		const { file } = await catResult.body.json();
-		interaction.reply({ files: [{ attachment: file, name: 'cat.png' }] });
+		interaction.editReply({ files: [{ attachment: file, name: 'cat.png' }] });
 	} else if (commandName === 'urban') {
 		const term = interaction.options.getString('term');
 		const query = new URLSearchParams({ term });
