defe36f9bf3c50e662823451f4c7c337b597cf85
Idris <idris.gamer.2006@gmail.com>
2022-11-18 20:51:05 +0100

    fix: add semicolon

--- code-samples/creating-your-bot/command-deployment/index.js
+++ code-samples/creating-your-bot/command-deployment/index.js
@@ -13,7 +13,7 @@ for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file)
+		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
