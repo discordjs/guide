6a6116f1d3d08bf7f287751c8627d4f1055591d7
Souji <timoqueezle@gmail.com>
2023-02-24 16:25:21 +0100

    feat: apply changes to resulting code section

--- code-samples/creating-your-bot/command-deployment/deploy-commands.js
+++ code-samples/creating-your-bot/command-deployment/deploy-commands.js
@@ -1,10 +1,12 @@
 const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
+const path = require('node:path');
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
