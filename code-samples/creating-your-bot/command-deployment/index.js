856c7dced1587789755c5e2dcc071bd94a033eab
almostSouji <timoqueezle@gmail.com>
2023-02-24 15:44:04 +0100

    fix(*): lint errors

--- code-samples/creating-your-bot/command-deployment/index.js
+++ code-samples/creating-your-bot/command-deployment/index.js
@@ -30,7 +30,7 @@ client.on(Events.InteractionCreate, async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		if(interaction.replied || interaction.deferred){
+		if (interaction.replied || interaction.deferred) {
 			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
 		} else {
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
