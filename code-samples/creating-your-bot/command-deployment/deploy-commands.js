34bddb55684d15d8a3589cb2bf316fba85c6b09f
Idris <idris.gamer.2006@gmail.com>
2023-02-25 21:41:18 +0100

    fix: eslint

--- code-samples/creating-your-bot/command-deployment/deploy-commands.js
+++ code-samples/creating-your-bot/command-deployment/deploy-commands.js
@@ -11,7 +11,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
 	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles =  fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
