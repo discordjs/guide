1b22fc7341d7a0ebd8449a9bd5240577033b8324
Idris <idris.gamer.2006@gmail.com>
2023-02-25 21:37:36 +0100

    fix: use path.join

--- code-samples/creating-your-bot/command-deployment/deploy-commands.js
+++ code-samples/creating-your-bot/command-deployment/deploy-commands.js
@@ -5,14 +5,16 @@ const path = require('node:path');
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
-const commandFolders = fs.readdirSync('./commands');
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
 	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
-		const command = require(`./commands/${folder}/${file}`);
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
 		commands.push(command.data.toJSON());
 	}
 }
