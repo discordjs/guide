5d54f3eb6d2ffc9d73682210af18be49644b212c
Idris <idris.gamer.2006@gmail.com>
2023-02-25 21:39:46 +0100

    fix: define commandsPath

--- code-samples/creating-your-bot/command-deployment/deploy-commands.js
+++ code-samples/creating-your-bot/command-deployment/deploy-commands.js
@@ -10,7 +10,8 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
-	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles =  fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
