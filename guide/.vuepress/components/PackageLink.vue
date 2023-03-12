0ae5fe209d00ad0588f211417426216663144b8b
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-03-12 21:14:51 +0000

    chore(*): update guide to 14.8.0 (#1424)

---
+++ guide/.vuepress/components/PackageLink.vue
@@ -0,0 +1,22 @@
+<template>
+	<a :href="link" target="_blank" rel="noopener noreferrer">
+		<code>{{ linkText }}</code><OutboundLink />
+	</a>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { defaultDjsBranch } from '../constants';
+
+const baseURL = `https://github.com/discordjs/discord.js/tree/${defaultDjsBranch}/packages`;
+
+const props = defineProps({
+	name: {
+		type: String,
+		required: true,
+	},
+});
+
+const link = computed(() => `${baseURL}/${props.name}`);
+const linkText = computed(() => `@discordjs/${props.name}`);
+</script>
