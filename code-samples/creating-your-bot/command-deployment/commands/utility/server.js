1465efe4147dade4377c6d0fd3fd1a2eb89981fe
mallaaiden <126505858+mallaaiden@users.noreply.github.com>
2023-04-03 16:21:54 +0000

    Main **Please describe the changes this PR makes and why it should be merged:**

--- code-samples/creating-your-bot/command-deployment/commands/utility/server.js
+++ code-samples/creating-your-bot/command-deployment/commands/utility/server.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('server')
 		.setDescription('Display info about this server.'),
 	async execute(interaction) {
-		return interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+		return interaction.reply(`***Server name:*** ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nWhat Server is About: The Lifestealer SMP is about a Minecraft Java server which is currently closed due to maintenance`);
 	},
 };
