91d1ae449e487f3b6b8763308c01b4877ecf259d
Nanashi <40239873+nanashi-03@users.noreply.github.com>
2023-05-04 05:04:17 +0530

    Update user.js
    Category

--- code-samples/additional-features/reloading-commands/commands/utility/user.js
+++ code-samples/additional-features/reloading-commands/commands/utility/user.js
@@ -4,6 +4,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('Provides information about the user.'),
+	category:"utility",
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
