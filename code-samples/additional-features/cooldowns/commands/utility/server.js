92c881c4a15edfe64a49d0cfc11bd34d6f4168fb
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-11-21 23:08:05 +0000

    fix: Add `cooldown` to all commands

--- code-samples/additional-features/cooldowns/commands/utility/server.js
+++ code-samples/additional-features/cooldowns/commands/utility/server.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+	cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('server')
 		.setDescription('Provides information about the server.'),
