c4eb9f1c0feca0c02b82722a0e451c18f9a6a667
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-11-21 22:00:18 +0000

    fix: cleanup page

--- code-samples/creating-your-bot/command-handling/index.js
+++ code-samples/creating-your-bot/command-handling/index.js
@@ -29,10 +29,12 @@ client.once(Events.ClientReady, readyClient => {
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
 
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
 
 	try {
 		await command.execute(interaction);
