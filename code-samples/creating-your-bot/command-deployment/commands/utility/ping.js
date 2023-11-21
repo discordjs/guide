6648019b1b96ab24049959d0e5177a5663872883
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-11-21 22:17:58 +0000

    fix(command-deployment): Cleanup page

--- code-samples/creating-your-bot/command-deployment/commands/utility/ping.js
+++ code-samples/creating-your-bot/command-deployment/commands/utility/ping.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		return interaction.reply('Pong!');
+		await interaction.reply('Pong!');
 	},
 };
