400d0dba36d79163effa8c4a13ba5302ede9aed0
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-11-28 11:29:37 +0000

    fix(command-deployment): Cleanup page (#1525)

--- code-samples/creating-your-bot/command-deployment/commands/utility/server.js
+++ code-samples/creating-your-bot/command-deployment/commands/utility/server.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
-		.setDescription('Display info about this server.'),
+		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
-		return interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
 	},
 };
--- guide/creating-your-bot/command-deployment.md
+++ guide/creating-your-bot/command-deployment.md
@@ -50,7 +50,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
-// Grab all the command files from the commands directory you created earlier
+// Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
